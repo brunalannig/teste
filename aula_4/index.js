acd095778d3fe87dabd1b4b18412d95890da9d21
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:54:16 -0300

    arquivo js atualizado

--- aula_4/index.js
+++ aula_4/index.js
@@ -2,3 +2,8 @@ console.log ("Hello World!")
 const nome = "Bruna"
 const idade = 23
 console.log ("Meu nome é ",nome," e tenho",idade," anos de vida")
+//teste
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
