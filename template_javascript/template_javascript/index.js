fb227743743efeb9bb2970738f948f2a72de86d1
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 12:11:40 -0300

    atusalizado

--- template_javascript/template_javascript/index.js
+++ template_javascript/template_javascript/index.js
@@ -11,12 +11,29 @@ alert(`Seu nome e ${nome} seu sobrenome e ${sobrenome} testando script`)
 
 let p = prompt("Quantas horas você trabalha por dia?")
 let t = prompt("Quanto você recebe por dia?")
-alert(`Voce recebe ${t/p} por hora`)*/
+alert(`Voce recebe ${t/p} por hora`)
 
-/*let nome
+let nome
 console.log(typeof nome)
-Esse tipo foi impresso pois não foi atribuido nenhum valor a variavel.*/
+Esse tipo foi impresso pois não foi atribuido nenhum valor a variavel.
 
 let nome = prompt("Qual é o seu nome?")
 let idade = prompt("Quantos anos você tem?")
-alert(`Ola ${nome}, você tem ${idade} anos`) 
+alert(`Ola ${nome}, você tem ${idade} anos`) */
+
+//let pergunta1 = ("Você está usando uma roupa azul hoje?")
+//let pergunta2 = ("O céu é azul?")
+//let pergunta3 = ("Você se considera bonito(a)")
+
+let a = 10
+let b = 25
+let c = 0
+
+//Troca desnecessário
+c = a
+a = b
+b = c
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
