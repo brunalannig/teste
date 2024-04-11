e7b8f8352c08fb0b48a7908152498556e07b7452
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 11:35:13 -0300

    arquvio js aula 11_04 atualizado

--- template_javascript/template_javascript/index.js
+++ template_javascript/template_javascript/index.js
@@ -3,12 +3,20 @@ const segundoNome = "Souza"
 const idade = 25
 const serEstudante = "Sim"
 console.log("O seu nome é",primeiroNome," o seu sobrenome é",segundoNome,"idade",idade,"é estudante? ", serEstudante)
-*/
 
-/*let nome = prompt("Qual e seu nome?")
+
+let nome = prompt("Qual e seu nome?")
 let sobrenome = prompt("Qual e seu sobrenome?")
-alert(`Seu nome e ${nome} seu sobrenome e ${sobrenome} testando script`)*/
+alert(`Seu nome e ${nome} seu sobrenome e ${sobrenome} testando script`)
 
 let p = prompt("Quantas horas você trabalha por dia?")
 let t = prompt("Quanto você recebe por dia?")
-alert(`Voce recebe ${t/p} por hora`)
+alert(`Voce recebe ${t/p} por hora`)*/
+
+/*let nome
+console.log(typeof nome)
+Esse tipo foi impresso pois não foi atribuido nenhum valor a variavel.*/
+
+let nome = prompt("Qual é o seu nome?")
+let idade = prompt("Quantos anos você tem?")
+alert(`Ola ${nome}, você tem ${idade} anos`) 
