2a75e1275555ff34cb8b07b49c58bc13a82a4b51
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 10:33:49 -0300

    arquivos editados

---
+++ template_javascript/template_javascript/index.js
@@ -0,0 +1,14 @@
+/*const primeiroNome = "Glauber"
+const segundoNome = "Souza"
+const idade = 25
+const serEstudante = "Sim"
+console.log("O seu nome é",primeiroNome," o seu sobrenome é",segundoNome,"idade",idade,"é estudante? ", serEstudante)
+*/
+
+/*let nome = prompt("Qual e seu nome?")
+let sobrenome = prompt("Qual e seu sobrenome?")
+alert(`Seu nome e ${nome} seu sobrenome e ${sobrenome} testando script`)*/
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
