84272f50ede1538474cd15dbc61d6661a8632b98
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-15 09:42:14 -0300

    teste commmit

--- template_javascript/template_javascript/index.js
+++ template_javascript/template_javascript/index.js
@@ -1,39 +1,24 @@
-/*const primeiroNome = "Glauber"
-const segundoNome = "Souza"
-const idade = 25
-const serEstudante = "Sim"
-console.log("O seu nome é",primeiroNome," o seu sobrenome é",segundoNome,"idade",idade,"é estudante? ", serEstudante)
-
-
-let nome = prompt("Qual e seu nome?")
-let sobrenome = prompt("Qual e seu sobrenome?")
-alert(`Seu nome e ${nome} seu sobrenome e ${sobrenome} testando script`)
-
-let p = prompt("Quantas horas você trabalha por dia?")
-let t = prompt("Quanto você recebe por dia?")
-alert(`Voce recebe ${t/p} por hora`)
-
-let nome
+//Recebeu as variáveis
+let nome = prompt("Digite seu nome: ") 
+let idade = prompt("Digite sua idade: ")
+//MENSAGEM: Digite as horas que você trabalhou por dia:
+let horasTrabalho = prompt("Digite as horas que você trabalha por dia")
+let salarioDia = prompt("Digite o seu salário diário")
+//Verificando as variaveis
 console.log(typeof nome)
-Esse tipo foi impresso pois não foi atribuido nenhum valor a variavel.
-
-let nome = prompt("Qual é o seu nome?")
-let idade = prompt("Quantos anos você tem?")
-alert(`Ola ${nome}, você tem ${idade} anos`) */
-
-//let pergunta1 = ("Você está usando uma roupa azul hoje?")
-//let pergunta2 = ("O céu é azul?")
-//let pergunta3 = ("Você se considera bonito(a)")
-
-let a = 10
-let b = 25
-let c = 0
-
-//Troca desnecessário
-c = a
-a = b
-b = c
-
-// Depois de trocados, teremos o seguinte resultado:
-console.log("O novo valor de a é", a) // O novo valor de a é 25
-console.log("O novo valor de b é", b) // O novo valor de b é 10
+console.log(typeof horasTrabalho)
+console.log(typeof salarioDia)
+//Convertendo o que é necessário
+let horasTrabNumber = Number(horasTrabalho)
+let salDiaNumber = Number(salarioDia)
+//Processando valores
+let valHoraNumber = horasTrabNumbe/salDiaNumber
+//Conversão para não dar erro de NULL
+let valorHora = String(valHoraNumber)
+const mensagem = ("Seu valor hora é", valorHora, "!!!")
+//Verificando variáveis
+console.log (typeof valorHora)
+console.log(typeof mensagem)
+console.log(mensagem)
+//Saída de valores para Front End
+alert(mensagem)
