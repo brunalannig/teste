653878575b1560e5b6d940e511d90c5404e3898a
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-15 10:06:15 -0300

    codigo atualizado

--- template_javascript/template_javascript/index.js
+++ template_javascript/template_javascript/index.js
@@ -6,16 +6,19 @@ let horasTrabalho = prompt("Digite as horas que você trabalha por dia")
 let salarioDia = prompt("Digite o seu salário diário")
 //Verificando as variaveis
 console.log(typeof nome)
+console.log(typeof idade)
 console.log(typeof horasTrabalho)
 console.log(typeof salarioDia)
 //Convertendo o que é necessário
+let nomeString = String(nome)
+let idadeString = String(idade)
 let horasTrabNumber = Number(horasTrabalho)
 let salDiaNumber = Number(salarioDia)
 //Processando valores
-let valHoraNumber = horasTrabNumbe/salDiaNumber
+let valHoraNumber = salDiaNumber/horasTrabNumber
 //Conversão para não dar erro de NULL
 let valorHora = String(valHoraNumber)
-const mensagem = ("Seu valor hora é", valorHora, "!!!")
+const mensagem = "Olá " + nome + "!" + " Seu valor hora é " + valorHora+ " !!! E você tem " + idade + " anos de idade."
 //Verificando variáveis
 console.log (typeof valorHora)
 console.log(typeof mensagem)
