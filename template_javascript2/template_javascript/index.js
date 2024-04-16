37c8620d38a16358c0dd268c4e09b4d201f24229
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 10:15:44 -0300

    atualizado

---
+++ template_javascript2/template_javascript/index.js
@@ -0,0 +1,33 @@
+/*const num1 = 3
+const num2 = 4
+console.log(num1 + num2)
+
+let resultado
+resultado = (3*5)/2
+alert(resultado)
+
+let resultado
+resultado = 234%5
+alert(resultado)*/
+
+//Declaração
+let primeiroNumero
+let segundoNumero
+let resultado
+
+//Lendo valor do usuário
+primeiroNumero = prompt ("Digite o primeiro número: ")
+segundoNumero = prompt ("Digite o segundo número: ")
+
+//Executando comparações e mostrando no Front
+resultado = primeiroNumero === segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero !== segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero < segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero > segundoNumero
+alert(resultado)
