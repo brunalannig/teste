a88a9ef573a012702b1bd0874ffb6a1e70a801cc
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 11:02:34 -0300

    novo arquivo editado

---
+++ template_javascript3/template_javascript/index.js
@@ -0,0 +1,33 @@
+//Declaração de variáveis
+let a, b, c, resultado
+
+//Leitura de dados informados pelo usuário
+a=true
+b=false
+c=true
+
+//1-Realize a operação: b && c
+
+resultado = a && b
+alert(resultado)
+
+resultado = b && c
+alert(resultado)
+
+resultado = a && c
+alert(resultado)
+
+resultado = a && b && c
+alert(resultado)
+
+resultado = a || b
+alert(resultado)
+
+resultado = b || c
+alert(resultado)
+
+resultado = a || c
+alert(resultado)
+
+resultado = a || b || c
+alert(resultado)
