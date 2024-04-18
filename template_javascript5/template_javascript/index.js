9bffffe8e98048b79c4fd26d54eb8a66c8e2ccea
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-18 09:54:21 -0300

    exercicio aula 18_04

---
+++ template_javascript5/template_javascript/index.js
@@ -0,0 +1,8 @@
+/*let nome = prompt("Qual é o seu nome?")
+let cor = prompt("Qual é a sua cor favorita?")
+alert(`Ola, seu nome é ${nome} e sua cor favorita é ${cor}.`)*/
+
+let nome = prompt("Qual é o seu nome?")
+let cor = prompt("Qual é a sua cor favorita?")
+const frase = "O seu nome é " + nome + "e sua cor favorita é " + cor
+alert(frase)
