47fe57631eb264ca51fc14e6a4607a1a47cf2392
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-18 11:54:10 -0300

    exercicios 18/04 atualizados

--- template_javascript5/template_javascript/index.js
+++ template_javascript5/template_javascript/index.js
@@ -1,8 +1,23 @@
 /*let nome = prompt("Qual é o seu nome?")
 let cor = prompt("Qual é a sua cor favorita?")
-alert(`Ola, seu nome é ${nome} e sua cor favorita é ${cor}.`)*/
+alert(`Ola, seu nome é ${nome} e sua cor favorita é ${cor}.`)
 
 let nome = prompt("Qual é o seu nome?")
 let cor = prompt("Qual é a sua cor favorita?")
 const frase = "O seu nome é " + nome + "e sua cor favorita é " + cor
 alert(frase)
+
+const frase = prompt("Digite uma frase: ")
+const novaFrase = (frase.replaceAll("o","i").toUpperCase())
+alert(novaFrase)
+console.log(frase.length)
+
+const inserirNumero = prompt("Digite um número de 0 a 7:")
+const racasCachorros = ["bulldog", "poodle", "beagle", "golden", "labrador", "rottweiler", "dalmata"]
+alert(racasCachorros[inserirNumero])*/
+
+const arrays = ["1" , "2", "3", "4", "5", "6"]
+console.log(arrays.length)
+console.log(arrays.push(7))
+console.log(arrays.splice(3,2))
+console.log(arrays.length)
