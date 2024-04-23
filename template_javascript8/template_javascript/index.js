4a0b188c5fee135e4cf26264f02849a26a5d6b3a
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 10:08:25 -0300

    5 exercicios condicionais

---
+++ template_javascript8/template_javascript/index.js
@@ -0,0 +1,18 @@
+/*let num1=25
+let num2=25
+
+if(num1===num2){
+    console.log("Sucesso! Números iguais")
+}
+else{
+    console.log("O número é diferente.")
+}*/
+
+let numUser = prompt("Digite um numero de 1 a 30: ")
+let numUser2 = prompt("Digite um numero de 1 a 30: ")
+if(numUser===numUser2){
+    alert(`${numUser == numUser2}. O numero é igual`) 
+}
+else{
+    alert(`${numUser == numUser2}. O numero é diferente!`)
+}
