4b98657a0de23d93098ba7352b325457a6921b8e
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 10:24:30 -0300

    exercicios condicionais atualizado

--- template_javascript8/template_javascript/index.js
+++ template_javascript8/template_javascript/index.js
@@ -6,7 +6,8 @@ if(num1===num2){
 }
 else{
     console.log("O número é diferente.")
-}*/
+}
+---------------------------------------------------
 
 let numUser = prompt("Digite um numero de 1 a 30: ")
 let numUser2 = prompt("Digite um numero de 1 a 30: ")
@@ -16,3 +17,35 @@ if(numUser===numUser2){
 else{
     alert(`${numUser == numUser2}. O numero é diferente!`)
 }
+
+---------------------------------------------------------
+
+let num1=30
+let num2=90
+if(num1===num2){
+    console.log("Números iguais")
+}else if (num1>num2) {
+    console.log(num1, "é maior que", num2)
+}else{
+    console.log(num1,"é menor que",num2)
+}
+
+--------------------------------------------- 
+
+let pokemonUser = prompt("Escolha um Pokémon entre Bulbassauro, Charmander e Squirtle: ")
+switch (pokemonUser){
+    case 'Bulbassauro':
+        alert("Este Pokémon é do tipo Planta e Veneno")
+        break
+    case 'Charmander':
+        alert("Este Pokémon é do tipo Fogo")
+        break
+    case 'Squirtle':
+        alert("Este Pokémon é do tipo Água")
+        break
+    default:
+        alert("Pokémon não encontrado")
+        break
+}
+
+-------------------------------------------------*/
