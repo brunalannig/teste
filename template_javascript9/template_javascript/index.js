cdc9818cd86ce6824c6b4913708b47084dcfa57b
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-24 09:25:04 -0300

    exercicios condicionais do enunciado

--- template_javascript9/template_javascript/index.js
+++ template_javascript9/template_javascript/index.js
@@ -5,3 +5,18 @@ if(idade>=18){
     alert('Não pode dirigir, benzinho :c')
 }*/
 
+let turnoAluno = prompt("Digite a letra correspondente ao turno em que você estuda: M (matutino), V (Vespertino) ou N (Noturno)")
+turnoAluno=turnoAluno.toUpperCase()
+if (turnoAluno=="M"){
+    console.log("Bom dia, querida(o)! c:")
+    alert("Bom dia, querida(o)! c:")
+} else if(turnoAluno=="V") {
+    console.log("Boa tarde, querida(o)! c:")
+    alert("Boa tarde, querida(o)! c:")
+} else if(turnoAluno=="N") {
+    console.log("Boa noite, querida(o)! c:")
+    alert("Boa noite, querida(o)! c:")
+} else {
+    console.log("Turno não identificado.")
+    alert("Turno não identificado.")
+}
