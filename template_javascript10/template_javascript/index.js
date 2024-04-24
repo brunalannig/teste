15fe2c7594d87965617856d7f9fb6ff51fc5f1d3
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-24 11:56:50 -0300

    exercicios laços

--- template_javascript10/template_javascript/index.js
+++ template_javascript10/template_javascript/index.js
@@ -1,7 +1,16 @@
-let numero = Number(prompt("Digite um número:"))
+/*let numero = Number(prompt("Digite um número:"))
 let soma = 0
 while(numero !==0){
     soma=soma+numero
     numero = Number(prompt("Digite um número:"))
 }
-alert(soma)
+alert(soma)*/
+
+let array = [11,15,18,14,12,13]
+let maiorValor=0
+for(let i=0; i<6; i++){
+    if(array[i] > maiorValor){
+        maiorValor=array[i]
+    }
+ }
+ console.log(maiorValor)
