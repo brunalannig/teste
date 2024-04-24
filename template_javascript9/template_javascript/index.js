c5871d7ae54f4195d450ed4baeef1fcf65077744
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-24 09:35:09 -0300

    exercicio de condicionais enunciado

--- template_javascript9/template_javascript/index.js
+++ template_javascript9/template_javascript/index.js
@@ -3,7 +3,7 @@ if(idade>=18){
     alert('Pode dirigir, meu bem c:')
 }else{
     alert('Não pode dirigir, benzinho :c')
-}*/
+}
 
 let turnoAluno = prompt("Digite a letra correspondente ao turno em que você estuda: M (matutino), V (Vespertino) ou N (Noturno)")
 turnoAluno=turnoAluno.toUpperCase()
@@ -20,3 +20,34 @@ if (turnoAluno=="M"){
     console.log("Turno não identificado.")
     alert("Turno não identificado.")
 }
+
+let turnoAluno = prompt("Digite a letra correspondente ao turno em que você estuda: M (matutino), V (Vespertino) ou N (Noturno)")
+turnoAluno=turnoAluno.toUpperCase()
+switch (turnoAluno){
+    case 'M':
+        alert("Bom dia, querida(o)! c:")
+        break
+    case 'V':
+        alert("Boa tarde, querida(o)! c:")
+        break
+    case 'N':
+        alert("Boa noite, querida(o)! c:")
+        break
+    default:
+        alert("Turno não identificado.")
+        break 
+    }*/
+
+    let generoFilme
+    let precoFilme
+    generoFilme = prompt("Digite o genêro do filme:")
+    precoFilme = Number(prompt ("Digite o preço do filme:"))
+    generoFilme = generoFilme.toUpperCase()
+
+    if((generoFilme=="Fantasia") && (precoFilme < 15)){
+        alert("Bom filme!")
+    }else{
+        alert("Escolha outro filme :(")
+    }
+
+
