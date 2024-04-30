96daf222ec477d62efabcb43509551aa3b9efd7d
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-30 12:00:09 -0300

    trabalho final

---
+++ template_javascript_TRABALHO_FINAL/template_javascript/index.js
@@ -0,0 +1,15 @@
+function imprimirMenu(){
+    let mensagem = ""
+    mensagem +="===== ABRIGOS TEMPORÁRIOS ====="
+    mensagem += "1. Cadastrar abrigo"
+    mesangem += "2. Listar abrigos"
+    mensagem += "3. Procurar abrigo"
+    mesangem += "4. Sair"
+    mensagem += "Escolha uma opção:"
+    let opcao = Number(prompt(mensagem))
+    return opcao
+}
+
+function main(){
+    
+}
